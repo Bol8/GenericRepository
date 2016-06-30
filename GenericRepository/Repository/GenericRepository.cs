4fb66d5dc63f6ab546ed60c62344a38400c1a09e
Oscar <Oscar@K380>
2016-06-30 18:26:41 +0200

    Add project files.

---
+++ GenericRepository/Repository/GenericRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using GenericRepository.Interfaces;
+using System.Data.Entity;
+
+namespace GenericRepository.Repository
+{
+    public abstract class GenericRepository<C,T> : IDisposable,
+        IGenericRepository<T> where T :class where C:DbContext,new()
+    {
+
+        private C _entities = new C();
+        public C Context
+        {
+
+            get { return _entities; }
+            set { _entities = value; }
+        }
+
+
+
+        public void Add(T entity)
+        {
+            _entities.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _entities.Set<T>().Remove(entity);
+        }
+
+        public void Dispose()
+        {
+            _entities.Dispose();
+        }
+
+        public void Edit(T entity)
+        {
+            _entities.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = _entities.Set<T>().Where(predicate);
+            return query;
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            IQueryable<T> query = _entities.Set<T>();
+            return query;
+        }
+
+
+        public void Save()
+        {
+            _entities.SaveChanges();
+        }
+
+
+
+
+    }
+}
